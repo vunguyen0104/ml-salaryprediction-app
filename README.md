0026d2312e34f2945b194c7fc93092bb0b01b99b
Vu Nguyen <nguyentranvu0104@gmail.com>
2021-12-03 09:41:55 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build A Salary Prediction Web App With Streamlit
+
+Build a Machine Learning web application from scratch in Python with Streamlit. We use real world data to build a machine learning model. The data is from 2020 Stackoverflow Developer Survey. In February 2020 they conduct the survey on nearly 65,000 developers on how they learn and level up, which tools they’re using, and what they want.
---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+
+# Emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+# This must be the first Streamlit command used in your app, and must only be set once. 
+st.set_page_config(
+    page_title="Software Developer Salary Prediction", 
+    page_icon=":chart_with_upwards_trend:", 
+    layout="wide"
+)
+
+page = st.sidebar.selectbox("Predict or Explore", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
